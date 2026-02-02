3bc750026dd6af813c47c2f8c1088ca9f6cd535e
Pawar Sushant Hanumant <sushantpawar1232@gmail.com>
2026-02-02 23:22:22 +0530

    88. Merge Sorted Array

---
+++ Day5.java
@@ -0,0 +1,37 @@
+// 88. Merge Sorted Array
+
+public class Day5 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1; // Pointer for nums1
+        int j = n - 1; // Pointer for nums2
+        int k = m + n - 1; // Pointer for merged array
+
+        // Merge in reverse order
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        // If there are remaining elements in nums2
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+    public static void main(String[] args) {
+        Day5 day5 = new Day5();
+
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+
+        day5.merge(nums1, m, nums2, n);
+
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
